6621c7991d896273624e36605491fd44e4ae2aee
Nomolos29 <nomolos2019@gmail.com>
2024-08-31 04:12:43 +0100

    Create state variable

--- contracts/ERC20TokenStakingContract.sol
+++ contracts/ERC20TokenStakingContract.sol
@@ -7,4 +7,15 @@ contract NomCoinPreSale {
   // State Variables 
   address owner;
   address tokenAddress;
+  uint public totalSupplyLeft;
+
+  struct Wallet {
+    uint stakedNomcoins;
+    uint stakeDuration;
+  }
+
+  constructor(address _tokenAddress) {
+    owner = msg.sender;
+    tokenAddress = _tokenAddress;
+  }
 }
