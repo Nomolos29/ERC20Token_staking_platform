4af95aa3806b68d5f4ffac21182076b8d5abb3ef
Nomolos29 <nomolos2019@gmail.com>
2024-08-31 04:21:54 +0100

    Custom Errors and Events

--- contracts/ERC20TokenStakingContract.sol
+++ contracts/ERC20TokenStakingContract.sol
@@ -24,4 +24,17 @@ contract NomCoinPreSale {
 
   // mapping reward amount to staker's address
   mapping (address => uint) internal rewardBalance;
+
+  // custom error
+  error ZeroAddressDetected();
+  error OngoingStake();
+  error totalSupplyMaxedOut();
+  error invalidInput();
+  error insufficientFunds();
+  error invalidTransaction();
+
+  // Events for transactions
+  event depositSuccessful(address indexed owner, uint indexed amount, uint indexed lockTime);
+  event withdrawalSuccessful(uint indexed amount, address indexed _address);
+  event withdrawAllFundSuccessful(uint indexed stakedAmount, uint indexed reward, uint indexed totalAmount);
 }
