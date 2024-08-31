5391133bcc1f059ff25155dea8a60f01bae93771
Nomolos29 <nomolos2019@gmail.com>
2024-08-31 01:17:52 +0100

    Added constructor

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -6,4 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract NomCoin is ERC20("NomCoin", "NCOIN") {
   // state variable
   address public owner;
+
+  constructor() {
+    owner = msg.sender;
+    _mint(msg.sender, 1000000e18);
+  }
 }
