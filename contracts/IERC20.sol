b6418321fe84b5c5adc9d90030bcfe0e236bac73
Nomolos29 <nomolos2019@gmail.com>
2024-08-31 01:16:23 +0100

    declare contract and state variables

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -1,3 +1,9 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract NomCoin is ERC20("NomCoin", "NCOIN") {
+  // state variable
+  address public owner;
+}
