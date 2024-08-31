abb2aa7cba6265aaf9af97df1eca6cc449b7c720
Nomolos29 <nomolos2019@gmail.com>
2024-08-31 04:29:38 +0100

    Modifiers and Basic Functions

--- contracts/ERC20TokenStakingContract.sol
+++ contracts/ERC20TokenStakingContract.sol
@@ -37,4 +37,26 @@ contract NomCoinPreSale {
   event depositSuccessful(address indexed owner, uint indexed amount, uint indexed lockTime);
   event withdrawalSuccessful(uint indexed amount, address indexed _address);
   event withdrawAllFundSuccessful(uint indexed stakedAmount, uint indexed reward, uint indexed totalAmount);
+
+  // checkers modifiers
+  function isSenderAddressZero() private view {
+    if (msg.sender == address(0)) { revert ZeroAddressDetected(); }
+  }
+
+  function stakeLocked() private view {
+    if (wallets[msg.sender].stakeDuration > block.timestamp) { revert OngoingStake(); }
+  }
+
+  function calculateReward(uint _days) private {
+    uint stakedBalance = wallets[msg.sender].stakedNomcoins;
+    uint reward = (stakedBalance / 200000) * _days;
+
+    totalSupplyLeft = IERC20(tokenAddress).balanceOf(tokenAddress);
+
+    if (reward <= totalSupplyLeft) { revert totalSupplyMaxedOut(); }
+
+    totalSupplyLeft -= reward;
+
+    rewardBalance[msg.sender] += reward;
+  }
 }
