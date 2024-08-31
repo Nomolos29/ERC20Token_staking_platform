0dab52a6b392c445cba0f10dbd893cdf89b1db53
Nomolos29 <nomolos2019@gmail.com>
2024-08-31 13:41:20 +0100

    Bugs fixed

--- contracts/ERC20TokenStakingContract.sol
+++ contracts/ERC20TokenStakingContract.sol
@@ -1,18 +1,20 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.26;
 
-import "./IERC20.sol";
+    import "./IERC20.sol";
 
-// **ERC20 Staking Smart Contract**
 
-// **Objective:** Write an ERC20 staking smart contract that allows users to stake a specific ERC20 token for rewards.
+    // **ERC20 Staking Smart Contract**
 
-// **Requirements:**
-// Users should be able to stake the ERC20 token by transferring the tokens to the contract.
-// The contract should track the amount and duration of each user’s stake.
-// Implement a reward mechanism similar to the Ether staking contract, where rewards are based on the staking duration.
-// Users should be able to withdraw their staked tokens and the rewards after the staking period.
-// The contract should handle ERC20 token transfers securely and efficiently.
+    // **Objective:** Write an ERC20 staking smart contract that allows users to stake a specific ERC20 token for rewards.
+
+    // **Requirements:**
+    // Users should be able to stake the ERC20 token by transferring the tokens to the contract.
+    // The contract should track the amount and duration of each user’s stake.
+    // Implement a reward mechanism similar to the Ether staking contract, where rewards are based on the staking duration.
+    // Users should be able to withdraw their staked tokens and the rewards after the staking period.
+    // The contract should handle ERC20 token transfers securely and efficiently.
+
 
 contract NomCoinMining {
   // State Variables 
